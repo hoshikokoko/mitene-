c0be43d5a01b9d46635383f8c6a29300d8fa06ca
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2023-01-12 09:22:38 +0000

    [Add]staff homes

---
+++ app/controllers/staff/homes_controller.rb
@@ -0,0 +1,4 @@
+class Staff::HomesController < ApplicationController
+  def top
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :staff do
+    get 'homes/top'
+  end
   devise_for :staffs,skip: [:passwords], controllers: {
     registrations: "staff/registrations",
     sessions: 'staff/sessions'
@@ -7,5 +10,7 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  root :to => "staff/homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
