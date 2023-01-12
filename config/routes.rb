568538f32ff15cb49bcd62709dc4fffa63ffbaf9
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2023-01-12 09:14:20 +0000

    [Add]devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :staffs
+  devise_for :staffs,skip: [:passwords], controllers: {
+    registrations: "staff/registrations",
+    sessions: 'staff/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
