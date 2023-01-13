86f596ea75f2542cb36d571b7c99ed6c600db30d
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2023-01-13 06:06:38 +0000

    [Add]model

---
+++ app/models/infomation_tag.rb
@@ -0,0 +1,2 @@
+class InfomationTag < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_12_084027) do
+ActiveRecord::Schema.define(version: 2023_01_13_060316) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -80,6 +80,13 @@ ActiveRecord::Schema.define(version: 2023_01_12_084027) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "infomation_tags", force: :cascade do |t|
+    t.integer "infomation_id"
+    t.integer "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "notifications", force: :cascade do |t|
     t.integer "infomation_id", null: false
     t.integer "infomation_comment_id", null: false
@@ -114,6 +121,12 @@ ActiveRecord::Schema.define(version: 2023_01_12_084027) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_staffs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_staffs_on_reset_password_token", unique: true
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
