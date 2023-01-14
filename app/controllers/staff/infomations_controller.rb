b832096f3685683d06693e47dc77b0950806dfa3
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2023-01-14 11:50:09 +0000

    [Add]infomation edit

--- app/controllers/staff/infomations_controller.rb
+++ app/controllers/staff/infomations_controller.rb
@@ -5,21 +5,20 @@ class Staff::InfomationsController < ApplicationController
   end
 
   def create
-    
     @infomation = Infomation.new(infomation_params)
     @infomation.staff_id = current_staff.id
     tag_list = params[:infomation][:name].split(nil)  
-    if @infomation.save                                         
-      @infomation.save_tag(tag_list)                         
-      redirect_to staff_top_path        
+    if @infomation.save
+      @infomation.save_tag(tag_list)
+      redirect_to staff_top_path
     else
       @tag_list = Tag.all
-      render :new        
+      render :new
     end
   end
 
   def index
-    @tag_list = Tag.all
+    @tag_lists = Tag.all
     @infomations = Infomation.all
   end
 
@@ -29,9 +28,25 @@ class Staff::InfomationsController < ApplicationController
   end
 
   def edit
+    @tag_lists = Tag.all
+    @infomation = Infomation.find(params[:id])
+    @tag_list = @infomation.tags.pluck(:name).join(' ')
   end
 
   def update
+    @infomation = Infomation.find(params[:id])
+    tag_list = params[:infomation][:name].split(nil)
+    if @infomation.update(infomation_params)
+      @old_infomation_tags = InfomationTag.where(infomation_id: @infomation.id)
+      @old_infomation_tags.each do |old|
+        old.delete
+      end
+      @infomation.save_tag(tag_list)
+
+      redirect_to staff_top_path
+    else
+      render :edit
+    end
   end
 
   def destroy
