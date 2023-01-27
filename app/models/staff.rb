56daf5ac8cf0d3cc064c2ac57c532f491e56f3a7
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2023-01-27 05:05:57 +0000

    [Fix]header

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -19,7 +19,7 @@ class Staff < ApplicationRecord
   # ゲストユーザーがない場合新規作成し探し出します。
   # パスワードはランダム生成されます。
   def self.gest
-    find_or_create_by(employee_number: '01') do |gest|
+    find_or_create_by(employee_number: 'gest') do |gest|
       gest.password = SecureRandom.urlsafe_base64
     end
   end
